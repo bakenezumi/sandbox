98fdc91b0275e731cc061ec72058e530995e2171
bakenezumi <hosonishi@gmail.com>
2018-09-17 21:32:33 +0900

    wip

--- src/main/scala/sandbox/delivery/VideoActivitySignal.scala
+++ src/main/scala/sandbox/delivery/VideoActivitySignal.scala
@@ -4,27 +4,27 @@ import java.nio.ByteBuffer
 
 import sandbox.fsm.{Signal, SignalLifeCycle}
 
-trait VideioActivitySignal extends Signal[VideoActivity] {
+trait VideoActivitySignal extends Signal[VideoActivity] {
   type ID = String
   type BODY = ByteBuffer
   val id: String
   val body: Option[ByteBuffer]
 }
-object VideioActivitySignal {
+object VideoActivitySignal {
 
   case class ViewProgress(id: String,
                           body: Option[ByteBuffer],
                           lifeCycle: SignalLifeCycle)
-      extends VideioActivitySignal
+      extends VideoActivitySignal
 
   case class ViewComplete(id: String,
                           body: Option[ByteBuffer],
                           lifeCycle: SignalLifeCycle)
-      extends VideioActivitySignal
+      extends VideoActivitySignal
 
   case class Click(id: String,
                    body: Option[ByteBuffer],
                    lifeCycle: SignalLifeCycle)
-      extends VideioActivitySignal
+      extends VideoActivitySignal
 
 }
--- src/main/scala/sandbox/delivery/VideoActivityState.scala
+++ src/main/scala/sandbox/delivery/VideoActivityState.scala
@@ -3,22 +3,22 @@ package sandbox.delivery
 import sandbox.fsm.DeferSignalState
 
 sealed trait VideoActivityState extends DeferSignalState[VideoActivity] {
-  override type SIGNAL = VideioActivitySignal
+  override type SIGNAL = VideoActivitySignal
 
   override def handleWithRerun(
-      signal: VideioActivitySignal): DeferSignalState[VideoActivity] =
+      signal: VideoActivitySignal): DeferSignalState[VideoActivity] =
     signal match {
-      case s: VideioActivitySignal.ViewProgress => handleViewProgress(s)
-      case s: VideioActivitySignal.ViewComplete => handleViewComplete(s)
-      case s: VideioActivitySignal.Click        => handleClick(s)
+      case s: VideoActivitySignal.ViewProgress => handleViewProgress(s)
+      case s: VideoActivitySignal.ViewComplete => handleViewComplete(s)
+      case s: VideoActivitySignal.Click        => handleClick(s)
     }
 
-  def handleViewProgress(signal: VideioActivitySignal.ViewProgress)
-    : DeferSignalState[VideoActivity]
-  def handleViewComplete(signal: VideioActivitySignal.ViewComplete)
-    : DeferSignalState[VideoActivity]
+  def handleViewProgress(
+      signal: VideoActivitySignal.ViewProgress): DeferSignalState[VideoActivity]
+  def handleViewComplete(
+      signal: VideoActivitySignal.ViewComplete): DeferSignalState[VideoActivity]
   def handleClick(
-      signal: VideioActivitySignal.Click): DeferSignalState[VideoActivity]
+      signal: VideoActivitySignal.Click): DeferSignalState[VideoActivity]
 
   def transitImpressed(signal: SIGNAL): DeferSignalState[VideoActivity] =
     transit(signal) { _ =>
@@ -30,7 +30,7 @@ sealed trait VideoActivityState extends DeferSignalState[VideoActivity] {
     }
 
   override def deadLetter(
-      signal: VideioActivitySignal): DeferSignalState[VideoActivity] = {
+      signal: VideoActivitySignal): DeferSignalState[VideoActivity] = {
     println("Dead letter:" + signal)
     this
   }
@@ -39,17 +39,17 @@ sealed trait VideoActivityState extends DeferSignalState[VideoActivity] {
 object VideoActivityState {
 
   case class UnDelivered(
-      initialDeferredSignals: Set[VideioActivitySignal] = Set(),
-      initialHandledSignals: Set[VideioActivitySignal] = Set())
+      initialDeferredSignals: Set[VideoActivitySignal] = Set(),
+      initialHandledSignals: Set[VideoActivitySignal] = Set())
       extends VideoActivityState {
-    override def handleViewProgress(signal: VideioActivitySignal.ViewProgress)
+    override def handleViewProgress(signal: VideoActivitySignal.ViewProgress)
       : DeferSignalState[VideoActivity] = ???
 
-    override def handleViewComplete(signal: VideioActivitySignal.ViewComplete)
+    override def handleViewComplete(signal: VideoActivitySignal.ViewComplete)
       : DeferSignalState[VideoActivity] = ???
 
     override def handleClick(
-        signal: VideioActivitySignal.Click): DeferSignalState[VideoActivity] =
+        signal: VideoActivitySignal.Click): DeferSignalState[VideoActivity] =
       ???
 
     override val stateId: Byte = 1
